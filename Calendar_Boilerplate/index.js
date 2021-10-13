2da7263cb78aa0eda19ffdce3293887dc7bd346e
june <khm11904@gmail.com>
2021-10-13 22:38:55 +0900

    Calendar 프로젝트 완성
    - 현재 월의 1일이 무슨 요일인지 판별하고, 해당 요일 라벨링에 1일 표기하기
    - 현재 월의마지막 날짜까지 달력에 표기하기
    - 우측 화살표를 클릭 했을때, 다음 달의 요일 및 날짜 표기
    - 좌측 화살표를 클릭 했을때, 이전 달의 요일 및 날짜 표기
    - 특정 날짜를 클릭 했을때, 상단의 요일 및 날짜 반영하기

--- Calendar_Boilerplate/index.js
+++ Calendar_Boilerplate/index.js
@@ -1,10 +1,5 @@
-const current_day = document.querySelector("#day");
-const current_date = document.querySelector("#date");
-const year_month = document.querySelector("#year-month");
-
-let date = new Date();
-const week = ["SUN", "MON", "THU", "WED", "THU", "FRI", "SAT"];
-const monthNames = [
+const WEEK = ["SUN", "MON", "THU", "WED", "THU", "FRI", "SAT"];
+const MONTHNAMES = [
   "JAN",
   "FEB",
   "MAR",
@@ -18,8 +13,99 @@ const monthNames = [
   "NOV",
   "DEC",
 ];
-current_day.innerHTML = week[date.getDay()];
+const MONTHDATE = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-current_date.innerHTML = date.getDate();
+const current_day = document.querySelector("#day");
+const current_date = document.querySelector("#date");
+const year_month = document.querySelector("#year-month");
+const columns = document.querySelector("thead").querySelectorAll("th");
+const rows = document.querySelector("tbody").querySelectorAll("tr");
+const left_button = document.querySelector("#left-button");
+const right_button = document.querySelector("#right-button");
 
-year_month.innerHTML = monthNames[date.getMonth()] + " " + date.getFullYear();
+let date = new Date();
+
+const init = function () {
+  current_day.innerHTML = WEEK[date.getDay()];
+  current_date.innerHTML = date.getDate();
+  year_month.innerHTML = MONTHNAMES[date.getMonth()] + " " + date.getFullYear();
+
+  clearRows();
+  displayDates(date.getMonth());
+
+  cellAddListener();
+};
+
+const displayDates = function (month) {
+  const date_num = date.getDate();
+  const last_date = MONTHDATE[month];
+
+  let row_index = 0;
+
+  for (let i = 1; i <= last_date; i++) {
+    date.setDate(i);
+    if (date.getDay() == 0) {
+      row_index++;
+    }
+    const cells = rows[row_index].querySelectorAll("td");
+
+    cells[date.getDay()].innerHTML = i;
+  }
+
+  date.setDate(date_num);
+};
+
+const clearRows = function () {
+  for (let tr of rows) {
+    const cells = tr.querySelectorAll("td");
+    for (let td of cells) {
+      td.innerHTML = "";
+    }
+  }
+};
+
+const leftButtonHandler = function () {
+  const month = date.getMonth() - 1;
+
+  date.setMonth(month, 1);
+
+  clearRows();
+  displayDates(date.getMonth());
+
+  current_date.innerHTML = date.getDate();
+  current_day.innerHTML = WEEK[date.getDay()];
+  year_month.innerHTML = MONTHNAMES[date.getMonth()] + " " + date.getFullYear();
+};
+
+const rightButtonHandler = function () {
+  const month = date.getMonth() + 1;
+
+  date.setMonth(month, 1);
+
+  clearRows();
+  displayDates(date.getMonth());
+
+  current_date.innerHTML = date.getDate();
+  current_day.innerHTML = WEEK[date.getDay()];
+  year_month.innerHTML = MONTHNAMES[date.getMonth()] + " " + date.getFullYear();
+};
+
+const cellHandler = function (event) {
+  date.setDate(event.target.innerHTML);
+  current_date.innerHTML = date.getDate();
+  current_day.innerHTML = WEEK[date.getDay()];
+};
+
+const cellAddListener = function () {
+  for (let tr of rows) {
+    const cells = tr.querySelectorAll("td");
+    for (let td of cells) {
+      td.addEventListener("click", cellHandler);
+    }
+  }
+};
+
+init();
+
+left_button.addEventListener("click", leftButtonHandler);
+right_button.addEventListener("click", rightButtonHandler);
